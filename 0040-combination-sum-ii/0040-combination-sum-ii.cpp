1ddf970a1804dd9f2e12eadd534e3483582a7385
Sachin Kumar <110785295+Sachin-Kushwaha1@users.noreply.github.com>
2024-06-05 03:55:46 +0530

    Time: 62 ms (5.08%), Space: 70.5 MB (5.01%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    void cnt(int n, vector<int>arr,int i, int sum, int& cs,vector<int>&v, vector<vector<int>>& ans)
+	{
+	    if(cs>sum) return;
+        if(i>=n)
+	    {
+	        if(cs==sum) ans.push_back(v);
+	        return;
+	    }
+	    cs += arr[i];
+        if(cs<=sum)
+        {
+
+            v.push_back(arr[i]);
+            cnt(n,arr,i+1,sum,cs,v,ans);
+            v.pop_back();
+        }
+	    cs -= arr[i];
+        int j=i;
+        while(j<arr.size() && arr[i]==arr[j]){
+            j++;
+        }
+        cnt(n,arr,j,sum,cs,v,ans); 
+	}
+    vector<vector<int>> combinationSum2(vector<int>& arr, int sum) {
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
+        vector<vector<int>> ans;
+        vector<int>v;
+        int cs=0;
+        cnt(n,arr,0,sum,cs,v,ans);
+        set<vector<int>>st(ans.begin(),ans.end());
+        ans.clear();
+        for(auto it:st)
+        {
+            ans.push_back(it);
+        }
+        return ans;
+    }
+};
