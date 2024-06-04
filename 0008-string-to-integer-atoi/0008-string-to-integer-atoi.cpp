a005e88064577a2adf680f829969f6043428af2a
Sachin Kumar <110785295+Sachin-Kushwaha1@users.noreply.github.com>
2024-06-04 18:35:12 +0530

    Time: 6 ms (23.05%), Space: 8.3 MB (36.95%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        deque<char>s1;
-        for(auto it:s)
-            s1.push_back(it);
-        while(s1.front() == ' ') s1.pop_front();
-        int i=0;
-        char sig;
-        if(s1.front() == '-' || s1.front() == '+')
-        {
-            sig = s1.front();
-            s1.pop_front();
-        }
-        while( s1.front() == '0') s1.pop_front();
-        string s2="";
-        while(!s1.empty())
-        {
-            s2 += s1.front();
-            s1.pop_front();
-        }
-        cout<<s2<<endl;
         long x=0;
-        i=0;
-        while( s2[i] >= '0' && s2[i] <= '9')
+        while(s[0] == ' ') s.erase(s.begin());
+        int i=0;
+        char ne;
+        if(s[0]=='-' || s[0]=='+')
         {
-            cout<<s2[i] -'0'<<endl;
-            x = 10*x + s2[i]-'0';
+            ne=s[0];
+            s.erase(s.begin());
+        }
+        while(s[0] == '0') s.erase(s.begin());
+        while(s[i] >= '0' && s[i] <= '9')
+        {
+            x = 10*x + s[i]-'0';
             ++i;
             if(x>INT_MAX) break;
         }       
-        if(sig == '-')
-            x *= -1;
+        if(ne == '-')
+            x*=(-1);
         if(x>INT_MAX) return INT_MAX;
-        if(x<INT_MIN) return INT_MIN;
+        if(x< INT_MIN) return INT_MIN;
         
         return x;
     }
