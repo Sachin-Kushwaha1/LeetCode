33535122df49d1f14898bd979901b21fe4ca2340
Sachin Kumar <110785295+Sachin-Kushwaha1@users.noreply.github.com>
2024-07-02 23:46:57 +0530

    Time: 0 ms (100%), Space: 12.6 MB (75.54%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,27 +1,41 @@
 class Solution {
 public:
-    vector<int> help(vector<int>& v1, vector<int>& v2)
-    {
-        vector<int> ans;
-        int j=0;
-        for(int i=0;i<v2.size() && j<v1.size();i++)
-        {
-            while(v2[i] > v1[j] && j<v1.size()) {
-                ++j;
-                if(j>=v1.size()) break;
-            }
-            if(j>=v1.size()) break;
-            if(v1[j] == v2[i]) {
-                ans.push_back(v1[j]); 
-                ++j;
-            }
-        }
-        return ans;
-    }
+    // vector<int> help(vector<int>& v1, vector<int>& v2)
+    // {
+    //     vector<int> ans;
+    //     int j=0;
+    //     for(int i=0;i<v2.size() && j<v1.size();i++)
+    //     {
+    //         while(v2[i] > v1[j] && j<v1.size()) {
+    //             ++j;
+    //             if(j>=v1.size()) break;
+    //         }
+    //         if(j>=v1.size()) break;
+    //         if(v1[j] == v2[i]) {
+    //             ans.push_back(v1[j]); 
+    //             ++j;
+    //         }
+    //     }
+    //     return ans;
+    // }
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         sort(nums1.begin(),nums1.end());
         sort(nums2.begin(),nums2.end());
-        if(nums1.size()>nums2.size()) return help(nums2,nums1);
-        return help(nums1,nums2);
+        int i = 0, j = 0;
+        vector<int> result;
+        
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                result.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        
+        return result;
     }
 };
