c5c7ac826bb14a7f9f98dcbba639cb2df585d754
Sachin Kumar <110785295+Sachin-Kushwaha1@users.noreply.github.com>
2024-08-03 00:28:53 +0530

    Time: 55 ms (96.66%), Space: 83.1 MB (47.74%) - LeetHub

--- 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -5,13 +5,11 @@ public:
         int totalone = accumulate(nums.begin(),nums.end(),0);
         if(totalone == n) return 0;
         int j=0;
-        int c1=0;
-        for(int i=0;i<totalone;i++)
-            if(nums[i]) ++c1;
+        int c1=accumulate(nums.begin(),nums.begin()+totalone,0);;
         int ans = totalone - c1;
-        for(int i=0, j=totalone; j<n && i<n ; i++, j = (j+1)%n)
+        for(int i=0; i<n ; i++)
         {
-            c1 += nums[j]-nums[i];
+            c1 += nums[(i+totalone)%n]-nums[i];
             ans = min(totalone-c1,ans);
         }
         return ans;
